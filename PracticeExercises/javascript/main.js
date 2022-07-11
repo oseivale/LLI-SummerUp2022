5f9ebe50832853fbbd0ca5446d2e04473096e8eb
Valerie Wireko Osei <valeriew@SML391TO.local>
2022-07-11 19:11:09 -0400

    adding day 5 content and work

---
+++ PracticeExercises/javascript/main.js
@@ -0,0 +1,18 @@
+// Declaring a variable
+var name = "Valerie";
+var city = "Scarborough";
+var month = 7;
+
+// Declaring an object
+var person = {
+  name: "John",
+  age: 20,
+  city: "Toronto",
+  hasPets: false,
+}
+
+// Declaring an array
+var shoppingList = ["Iced Tea", "Onions", "Juice", "Tofu", "Bread"]
+
+console.log(shoppingList)
+console.log(person.city)
